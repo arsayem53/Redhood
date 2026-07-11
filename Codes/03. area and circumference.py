77dac559ed3aa4ab2c0ab48e15494eb72fc26b87
arsayem53 <karsayem@gmail.com>
2026-07-11 14:12:42 +0600

    Code 4.

---
+++ Codes/03. area and circumference.py
@@ -0,0 +1,7 @@
+#program to read the radius of a circle and print its area and circumference.
+pi = 3.1416
+r = float(input("Enter Radius: "))
+area = pi * r * r
+circumference = 2 * pi * r
+print(f"Area: {area}")
+print(f"Circumference: {circumference}")
