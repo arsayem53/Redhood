1a3b31ad743d46d3745abebcf2792b22468d155a
arsayem53 <karsayem@gmail.com>
2026-07-11 23:44:08 +0600

    Code 19.

---
+++ Codes/19. Divisible by 3 and 5.py
@@ -0,0 +1,5 @@
+x = int(input("Enter a number: "))
+if x % 3 == 0 and x % 5 == 0:
+    print("The number is divisible by both 3 and 5.")
+else:
+    print("The number is not divisible by both 3 and 5.")
