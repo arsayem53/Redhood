990c19430195499f6b6a84792d23e7f9095dc9c5
arsayem53 <karsayem@gmail.com>
2026-07-11 23:00:34 +0600

    Code 16.

--- Codes/16. Vowel or Consonant.py
+++ Codes/16. Vowel or Consonant.py
@@ -2,5 +2,6 @@ target = input("Enter Char: ")
 if target == 'a' or target == 'e' or target == 'i' or target == 'o' or target == 'u' or target == 'A' or target == 'E' or target == 'I' or target == 'O' or target == 'U':
      print("This is a vowel")
 else:
-     print("This is a consonant")
+     print("This is consonant")
+
  
