0f663022de3c3a3171fe351c23ae7ac5fc2589e1
arsayem53 <karsayem@gmail.com>
2026-07-11 20:16:07 +0600

    Code 12.

---
+++ Codes/12. Positive Negative or Zero.py
@@ -0,0 +1,10 @@
+#positive or nefgative or zero
+
+num = input(int("Enter a number: "))
+if num == 0:
+    print("The number is zero.")
+elif num > 0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
+    
