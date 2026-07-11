86ed0fe9ff375ef7fd0f2ed29f4758882b104a04
arsayem53 <karsayem@gmail.com>
2026-07-11 14:59:34 +0600

    Code 5.

---
+++ Codes/05. Rectangle.py
@@ -0,0 +1,7 @@
+#program to read the length and breadth of a rectangle and print its area and perimeter.
+l = int(input("Length :"))
+b = int(input("Breadth :"))
+area = l * b
+perimeter = 2 * (l + b)
+print(f"Area : {area}")
+print(f"Perimeter : {perimeter}")
