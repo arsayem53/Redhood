2d3156e201d8d1cd4b6ec95e28355d0a16304ee7
arsayem53 <karsayem@gmail.com>
2026-07-11 15:12:25 +0600

    Code 6.

---
+++ Codes/06. Swap two numbers using third variable.py
@@ -0,0 +1,8 @@
+#swap 2 number with third variable
+a = int(input("ENter number 1: "))
+b = int(input("Enter number 2: "))
+print("Before swapping: a =", a, "b =", b)
+temp = a
+a = b
+b = temp
+print("After swapping: a =", a, "b =", b)
