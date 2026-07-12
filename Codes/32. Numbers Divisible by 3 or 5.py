3563996180ad89f818743fd75793778b46ff6b20
arsayem53 <karsayem@gmail.com>
2026-07-12 23:33:41 +0600

    Code 32.

--- Codes/32. Numbers Divisible by 3 or 5.py
+++ Codes/32. Numbers Divisible by 3 or 5.py
@@ -4,4 +4,4 @@ count = 0
 for i in range(1, n + 1):
     if i % 3 == 0 or i % 5 == 0:
         count += 1
-print("Count of numbers divisible by 3 from 1 to", n, "is:", count)
+print("Count of numbers divisible by 3 or 5 from 1 to", n, "is:", count)
