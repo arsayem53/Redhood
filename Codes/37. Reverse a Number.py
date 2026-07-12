fcc4eb20f9070f92411c0a5a7364a23668e3fb61
arsayem53 <karsayem@gmail.com>
2026-07-12 23:40:10 +0600

    Code 37.

---
+++ Codes/37. Reverse a Number.py
@@ -0,0 +1,8 @@
+#reverse a number
+n = int(input("Enter a number: "))
+reverse = 0
+while n > 0:
+    digit = n % 10
+    reverse = reverse * 10 + digit
+    n = n // 10
+print("Reverse of the number is:", reverse)
